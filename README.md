c52c599baaa83224b4268c23a1bb306b38dbab40
Vamsi Eyunni <vamsieyunni@gmail.com>
2024-12-31 20:18:03 -0800

    working example

---
+++ README.md
@@ -0,0 +1,5 @@
+# Forward Forward on MNIST
+
+Clone of [this repo](https://github.com/mpezeshki/pytorch_forward_forward) heavily documented.
+
+[Original Paper](https://arxiv.org/abs/2212.13345)
--- f2.py
+++ f2.py
@@ -1,4 +1,30 @@
 import torch
+from torchvision.transforms import Compose, ToTensor, Normalize, Lambda
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+
+def gen_mnist_loaders(train_batch_size=50000, test_batch_size=10000):
+
+    transform = Compose([
+        ToTensor(),
+        # MNIST (mean pixel value, std dev pixel value) for normalization.
+        Normalize((0.1307,), (0.3081,)),
+        Lambda(lambda x: torch.flatten(x))
+    ])
+
+    train_loader = DataLoader(
+        MNIST('./data/', train=True, download=True, transform=transform),
+        batch_size=train_batch_size,
+        shuffle=True
+    )
+
+    test_loader = DataLoader(
+        MNIST('./data/', train=False, download=True, transform=transform),
+        batch_size=test_batch_size,
+        shuffle=False
+    )
+
+    return train_loader, test_loader
 
 def overlay_y_on_x(x, y):
 
@@ -22,7 +48,7 @@ def overlay_y_on_x(x, y):
 
 class Network(torch.nn.Module):
     
-    def __init___(self, dims):
+    def __init__(self, dims):
         
         super().__init__()
         
@@ -30,15 +56,73 @@ class Network(torch.nn.Module):
         for d in range(len(dims) - 1):
             self.layers += [Layer(dims[d], dims[d+1])]
 
+    # the way the network is defined we technically
+    # don't have the last layer of neurons that output
+    # the prediction itself. I think that the predict
+    # function is trying to emulate the last layer of 
+    # 10 fully connected neurons.
+
     def predict(self, x):
 
+        # this is the sum of activations for each 0-9 label
         goodness_per_label = []
 
-        # mnist has 10 handwritten digits
+        # for each example, randomly apply a label then feed 
+        # it through the nerual net. the label with the highest
+        # goodness value is the predicted label.
+
         for label in range(10):
 
-            # h = overlay_y_on_x
-            pass
+            h = overlay_y_on_x(x, label)
+            goodness_per_layer = []
+
+            for layer in self.layers:
+
+                # iterate through each layer and accumulate the goodness
+                # value for each layer. the goodness value here is just
+                # calling the forward pass, then normalizing the 
+                # activiations.
+
+                # h is a vector of the activations from the current layer.
+                # this is then fed into the next layer until we reach the
+                # very end of the network. When you call an object directly
+                # python invokes the object's __call__ method, which
+                # implicitly calls the forward(...) function. The forward()
+                # function takes the input dimension, performs the 
+                # computation and returns a vector with length equal
+                # to the specified output dimension.
+
+                # initially h has the shape (batch_size, 784) which
+                # gets reduced to (batch_size, 500) in the next iter.
+                h = layer(h)
+
+                # compute the goodness vector for the given example.
+                # this will have shape of (batch_size,)
+                current_layer_goodness_vector = h.pow(2).mean(1)
+
+                # accumulate goodness vectors for each layer given the
+                # current label. this will contain as many vectors
+                # as layers, and each vector has shape (batch_size,)
+                # the array itself will be of shape (layer_count, batch_size)
+                # by the end of the loop
+                goodness_per_layer += [current_layer_goodness_vector]
+            
+            # for this hardcoded example these tensors are all size
+            # the shape will be (batch_size, 1) in this example.
+            goodness_per_label += [sum(goodness_per_layer).unsqueeze(1)]
+
+        # figure out which label was the "best fit" for
+        # the provided example. in this example the 
+        # TODO: this is really confusing
+        goodness_per_label = torch.cat(goodness_per_label, 1)
+        return goodness_per_label.argmax(1)
+
+    def train(self, x_pos, x_neg):
+
+        h_pos, h_neg = x_pos, x_neg
+        for i, layer in enumerate(self.layers):
+            print('training layer', i, '...')
+            h_pos, h_neg = layer.train(h_pos, h_neg)
 
 # torch.nn.Linear is used to implement a dense
 # (fully connected) layer. It applies a linear
@@ -46,7 +130,7 @@ class Network(torch.nn.Module):
 # y = xA^T + b [weight and bias].
 class Layer(torch.nn.Linear):
 
-    def __init__(in_features, out_features):
+    def __init__(self, in_features, out_features):
 
         # the bias term allows the neuron to learn to activate even
         # when the weighted input is very small. Prevents "dead" neurons.
@@ -118,7 +202,7 @@ class Layer(torch.nn.Linear):
             # we don't technically need the error vector itself as we only care about
             # the gradient since that is what updates the weights and biases. The error
             # vector values are useful for monioring progress and such.
-            smooth_relu_error_vector = torch.log(1 + torch.exp(pos_neg_goodness_error_vector))
+            loss = torch.log(1 + torch.exp(pos_neg_goodness_error_vector)).mean()
 
             # zero out the gradients from the previous batch, so we can start
             # fresh for the current batch. pytorch by default accumulates
@@ -129,7 +213,7 @@ class Layer(torch.nn.Linear):
             # to the vector. therefore when you call backward() it is able to go
             # through the graph and compute the gradient of the vector. this function
             # call stores the gradient in the .grad portion of the weights and biases.
-            smooth_relu_error_vector.backward()
+            loss.backward()
 
             # the optimizer uses the .grad portion of the weights and biases to update
             self.optimizer.step()
@@ -139,4 +223,24 @@ class Layer(torch.nn.Linear):
         return self.forward(x_pos).detach(), self.forward(x_neg).detach()
 
 if __name__ == "__main__":
-    pass
+
+    torch.manual_seed(1234)
+    train_loader, test_loader = gen_mnist_loaders()
+
+    network = Network([784, 500, 500])
+
+    # load the positive examples
+    x, y = next(iter(train_loader))
+    x_pos = overlay_y_on_x(x, y)
+
+    # load the negative examples
+    # TODO: no idea how these two lines work
+    rnd = torch.randperm(x.size(0))
+    x_neg = overlay_y_on_x(x, y[rnd])
+
+    network.train(x_pos, x_neg)
+    print('train error:', 1.0 - network.predict(x).eq(y).float().mean().item())
+
+    # load test dataset
+    x_te, y_te = next(iter(test_loader))
+    print('test error:', 1.0 - network.predict(x_te).eq(y_te).float().mean().item())
